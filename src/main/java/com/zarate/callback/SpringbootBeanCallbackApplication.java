f8a1d11c2277035805729732583e9dd63a440db9
joaozarate <joaozarate@gmail.com>
2020-04-13 20:00:32 +0100

    Project created

---
+++ src/main/java/com/zarate/callback/SpringbootBeanCallbackApplication.java
@@ -0,0 +1,13 @@
+package com.zarate.callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBeanCallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBeanCallbackApplication.class, args);
+	}
+
+}
